c97fbc87628cd3d157790a202ce4549cc59bf94b
Peter Ramos <peterdramos02@gmail.com>
2019-02-26 15:41:12 -0500

    complete trangle and  reverse str iteration big O drills

--- iterative-big-O-drills.js
+++ iterative-big-O-drills.js
@@ -1,7 +1,7 @@
 // Exercise 1 -- Counting Sheep
 function countSheepLoop(num){
   for(let i=num; i>0; i--){
-      console.log(`counting sheeps ${i}`);
+    console.log(`counting sheeps ${i}`);
   }
 }
 countSheepLoop(10);
@@ -14,7 +14,7 @@ countSheepLoop(10);
 function double_all(arr) {
   var ret = Array(arr.length);
   for (var i = 0; i < arr.length; ++i) {
-      ret[i] = arr[i] * 2;
+    ret[i] = arr[i] * 2;
   }
   return ret;
 }
@@ -22,5 +22,33 @@ let arr = [10,4,5,2,1];
 console.log(double_all(arr));
 
 // This algorithm is O(n) -- Linear Time complexity because it runs through each index in the array once, so the run times in direct proportion to the input
+
+// =================================================
+
+//Exercise 3: Take a string as input, reverse the string, and return the new
+function reverse_tail(str) {
+  var accumulator = '';
+  while (str !== '') {
+    accumulator = str[0] + accumulator;
+    str = str.slice(1);
+  }
+  return accumulator;
+}
+
+// This algorithm is O(n) -- Linear time-complexity because there will be as many operations as there are elements in the string input
+
+// =================================================
+
+//Exercise 4: Calculates the nth triangular number.
+function triangle(n) {
+  var tot = 0;
+  for (var i = 1; i <= n; ++i) {
+    tot += n;
+  }
+  return tot;
+}
+
+// This algorithm is O(n) -- Linear time-complexity because the for loop is going to run for as long as n is big and just sum up
+// the numbers and return that. Input directly proportional to time
 
 // =================================================
